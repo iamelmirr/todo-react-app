dcec4179424a6639a6f1e0d22cc6a1f65ad05a27
iamelmirr <besirovicelmir36@gmail.com>
2024-12-03 21:34:15 +0100

    todo app v1

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,16 @@
+export function Tabs() {
+    const tabs = ['All', 'Open', 'Completed']
+    return (
+        <nav>
+
+            {tabs.map((tab, tabIndex) => {
+                return (
+                    <button key={tabIndex}>
+                        <h4>{tab}</h4>
+                    </button>
+                )
+            })}
+
+        </nav>
+    )
+}
